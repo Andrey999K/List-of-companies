c541e20de5f66be0067806023612558ff6b6774c
Andrey999K <andrey1234xxxx@mail.ru>
2024-02-21 17:34:47 +0300

    feat: add company.services.ts

---
+++ src/services/company.services.ts
@@ -0,0 +1,30 @@
+import httpService from "./http.services.ts";
+import convertQueryParams from "../utils/convertQueryParams.ts";
+import { ObjectDefault } from "../types/types.ts";
+
+const botMessagesEndPoint = "category";
+
+const companyService = {
+  get: async (body?: ObjectDefault) => {
+    const { data } = body
+      ? await httpService.get(botMessagesEndPoint + convertQueryParams(body))
+      : await httpService.get(botMessagesEndPoint);
+    return data;
+  },
+  post: async (body: ObjectDefault) => {
+    const { data } = await httpService.post(botMessagesEndPoint, body);
+    return data;
+  },
+  patch: async (body: ObjectDefault, config: Object) => {
+    const { data } = config
+      ? await httpService.put(botMessagesEndPoint, body, config)
+      : await httpService.put(botMessagesEndPoint, body);
+    return data;
+  },
+  delete: async (body: ObjectDefault) => {
+    const { data } = await httpService.delete(botMessagesEndPoint + convertQueryParams(body));
+    return data;
+  }
+};
+
+export default companyService;

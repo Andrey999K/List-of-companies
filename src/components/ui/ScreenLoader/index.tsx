7385bd00c5b597f7023b6d1fbd3ff3a207deaffd
Andrey999K <andrey1234xxxx@mail.ru>
2024-02-21 16:41:14 +0300

    feat: add position for mock data employees

--- src/components/ui/ScreenLoader/index.tsx
+++ src/components/ui/ScreenLoader/index.tsx
@@ -6,7 +6,7 @@ const ScreenLoader = () => {
       className={`fixed top-0 bottom-0 right-0 left-0 w-screen h-[100dvh] bg-white/90 flex justify-center items-center z-[9999]`}
     >
       {/*<PuffLoader color={darkTheme ? "#FFFFFF" : "#1F1F21"} />*/}
-      загрузка
+      Загрузка
     </div>
   );
 };

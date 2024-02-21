dc407e60a1932d3e7fc8b9dd77350b34e4fcabcc
Andrey999K <andrey1234xxxx@mail.ru>
2024-02-21 15:14:42 +0300

    feat: add slicer for employee

--- src/store/employeeSlicer.ts
+++ src/store/employeeSlicer.ts
@@ -1,62 +1,62 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import companyList from "../mock/companyList.json";
+import employeeList from "../mock/employeeList.json";
 import httpService from "../services/http.services.ts";
 import isDev from "../utils/isDev.ts";
-import { Company, InitialState, Request } from "../types/types.ts";
+import { Employee, InitialState, Request } from "../types/types.ts";
 
-type InitialStateCompany = InitialState<Company[]>;
+type InitialStateEmployee = InitialState<Employee[]>;
 
 type State = {
-  company: InitialStateCompany;
+  employee: InitialStateEmployee;
 };
 
-export const requestCompanyList = createAsyncThunk("companyList/request", async (_, { rejectWithValue }) => {
+export const requestEmployeeList = createAsyncThunk("employeeList/request", async (_, { rejectWithValue }) => {
   try {
-    const { result } = (await httpService.get("categories")) as Request<Company[]>;
+    const { result } = (await httpService.get("employees")) as Request<Employee[]>;
     if (result.status === "200") return result.data;
     return [];
   } catch (error: any) {
     if (isDev()) {
       console.log(error);
-      return companyList as Company[];
+      return employeeList as Employee[];
     }
     return rejectWithValue(error.message);
   }
 });
 
-const setPending = (state: InitialStateCompany) => {
+const setPending = (state: InitialStateEmployee) => {
   state.isLoading = true;
   state.error = null;
 };
 
-const setRejected = (state: InitialStateCompany, { payload }: { payload: any }) => {
+const setRejected = (state: InitialStateEmployee, { payload }: { payload: any }) => {
   state.isLoading = false;
   state.error = payload;
 };
 
-const initialState: InitialState<Company[]> = {
+const initialState: InitialState<Employee[]> = {
   entities: [],
   isLoading: false,
   error: null
 };
 
-const companySlice = createSlice({
-  name: "company",
+const employeeSlice = createSlice({
+  name: "employee",
   initialState,
   reducers: {},
   extraReducers: builder => {
-    builder.addCase(requestCompanyList.pending, setPending);
-    builder.addCase(requestCompanyList.fulfilled, (state: InitialStateCompany, { payload }) => {
+    builder.addCase(requestEmployeeList.pending, setPending);
+    builder.addCase(requestEmployeeList.fulfilled, (state: InitialStateEmployee, { payload }) => {
       state.isLoading = false;
       if (payload) state.entities = payload;
     });
-    builder.addCase(requestCompanyList.rejected, setRejected);
+    builder.addCase(requestEmployeeList.rejected, setRejected);
   }
 });
 
-const { reducer: companyReducer } = companySlice;
+const { reducer: employeeReducer } = employeeSlice;
 
-export const getCompanyList = () => (state: State) => state.company.entities;
-export const getCompanyLoading = () => (state: State) => state.company.isLoading;
+export const getEmployeeList = () => (state: State) => state.employee.entities;
+export const getEmployeeLoading = () => (state: State) => state.employee.isLoading;
 
-export default companyReducer;
+export default employeeReducer;
